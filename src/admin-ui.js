aa79411d7b4dae3961b6543efc3d6fb87c031bf4
Paweł Oślizło <fluffyslvt@gmail.com>
2023-05-18 16:11:15 +0200

    new file:   src/admin-ui.js     added: admin logic

---
+++ src/admin-ui.js
@@ -0,0 +1,33 @@
+const addProductForm = document.querySelector(".form-add-product");
+const nameInput = document.querySelector('[name="product-name"]');
+const priceInput = document.querySelector('[name="product-price"]');
+const productsUl = document.querySelector(".products-list");
+
+const addProductToShop = (e) => {
+  e.preventDefault();
+
+  const name = nameInput.value;
+  const price = Number(priceInput.value);
+
+  const newLi = document.createElement("li"); // <li>
+
+  const newStrong = document.createElement("strong");
+  newStrong.innerText = name;
+
+  const newPriceText = document.createTextNode(` - ${price.toFixed(2)}`);
+
+  const newBtn = document.createElement("button");
+  newBtn.classList.add("btn-buy-product");
+  newBtn.dataset.name = name;
+  newBtn.dataset.price = String(price);
+  newBtn.innerText = "Kup";
+  newBtn.addEventListener("click", addProductToBasket);
+
+  newLi.appendChild(newStrong);
+  newLi.appendChild(newPriceText);
+  newLi.appendChild(newBtn);
+
+  productsUl.appendChild(newLi);
+};
+
+addProductForm.addEventListener("submit", addProductToShop);
